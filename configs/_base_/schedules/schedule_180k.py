c5779be0cf20b5bd2ba0079368f9e6e46f65e006
DINGJIE PENG <43697512+jungletada@users.noreply.github.com>
2024-03-14 12:48:53 +0800

    Add files via upload

---
+++ configs/_base_/schedules/schedule_180k.py
@@ -0,0 +1,29 @@
+max_iters = 180000
+# optimizer
+optimizer=dict(type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+# learning policy
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=max_iters,
+        by_epoch=False,
+    )
+]
+
+train_cfg = dict(
+    type='IterBasedTrainLoop', max_iters=max_iters, val_interval=max_iters // 10)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=max_iters // 10),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
---
+++ configs/adapt_mit_custom/adapt_mit_b1_coco_stuff164k.py
@@ -0,0 +1,54 @@
+_base_ = [
+    '../_base_/models/adapt_segformer_b0.py',
+    '../_base_/datasets/coco-stuff164k.py',
+    '../_base_/default_runtime.py', 
+    '../_base_/schedules/schedule_80k.py'
+]
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b1_20220624-02e5a6a1.pth'  # noqa
+
+num_classes = 171
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+sem_norm_cfg = dict(type='LN2d', requires_grad=True, eps=1e-6)
+
+model = dict(
+    data_preprocessor=data_preprocessor,
+    pretrained=checkpoint, 
+    backbone=dict(
+        embed_dims=64, 
+        aux_ratio=[3, 3, 3, 3], 
+        num_aux_layers=[1, 1, 1, 1], 
+        sem_norm_cfg=sem_norm_cfg,
+        num_classes=num_classes),
+    decode_head=dict(
+        in_channels=[64, 128, 320, 512],
+        num_classes=num_classes))
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.),
+            'aux_layers': dict(lr_mult=7.),
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=160000,
+        by_epoch=False,
+    )
+]
+train_dataloader = dict(batch_size=8, num_workers=8)
+val_dataloader = dict(batch_size=1, num_workers=4)
+test_dataloader = val_dataloader
